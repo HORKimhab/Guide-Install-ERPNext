e5057373aaabae7b2a296161dfbfa8adfd04beec
HKimhab <kimhab.zeecode@gmail.com>
2023-12-31 15:58:18 +0700

    init: input password till mactch, if not input again...

--- erpnext-on-ubuntu22.04.sh
+++ erpnext-on-ubuntu22.04.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Variables
-USERNAME="frappe"
-FULLNAME="Frappe"
+USERNAME="frappe4"
+FULLNAME="Frappe4"
 HOMEDIR="/home/$USERNAME"
 
 echo "Install ERPNext on Ubuntu 22.04"
@@ -18,8 +18,29 @@ sudo apt update -y && sudo apt upgrade -y
 # Create user without giving password 
 sudo adduser --gecos "$FULLNAME" --disabled-password --home "$HOMEDIR" "$USERNAME"
 
-# Set password
-sudo passwd "$USERNAME" # Password check in general doc
+# Set password, if new and confirm password not match, input it again till match
+while true; do
+    read -s -p "Enter the desired password for $USERNAME: " NEW_PASSWORD
+    echo
+    read -s -p "Confirm password: " CONFIRM_PASSWORD
+    echo
+
+    if [ "$NEW_PASSWORD" == "$CONFIRM_PASSWORD" ]; then
+        echo "Password successfully set."
+        sudo passwd "$USERNAME" <<< "$NEW_PASSWORD" # Password check in general doc
+        break
+    else
+        echo "Passwords do not match. Please try again."
+    fi
+done
+
+# Check if the password change was successful
+if [ $? -eq 0 ]; then
+    echo "Password successfully set."
+else
+    echo "Error: Password set failed."
+    exit 1
+fi
 
 # Add user to sudo group (if needed)
 sudo usermod -aG sudo "$USERNAME"
